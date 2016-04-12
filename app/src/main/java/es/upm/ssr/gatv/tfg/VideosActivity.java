9c825de3acc1a8259e967fb20ce156ca946674df
alejandro-flocoello <alejandro.flocoello@gmail.com>
2016-04-12 20:03:22 +0200

    Videos Notification

--- app/src/main/java/es/upm/ssr/gatv/tfg/VideosActivity.java
+++ app/src/main/java/es/upm/ssr/gatv/tfg/VideosActivity.java
@@ -3,36 +3,34 @@ package es.upm.ssr.gatv.tfg;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import es.upm.ssr.gatv.tfg.Entry;
 
 public class VideosActivity extends  AppCompatActivity {
 
     private AdaptadorClass mAdapter;
     private ListView entryList;
-
+    private MediaPlayer mMediaPlayer;
 
 
 
@@ -96,9 +94,19 @@ public class VideosActivity extends  AppCompatActivity {
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
+    public void onPostResume(){
+        if (mMediaPlayer != null){
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+        }
+        super.onPostResume();
+    }
+
     private class EntryDownloadTask extends AsyncTask<Void, Void, Void> {
 
         public boolean server;
+        private int posactual = 0;
+        private int posant = 0;
 
     @Override
     protected Void doInBackground(Void... arg0) {
@@ -118,13 +126,19 @@ public class VideosActivity extends  AppCompatActivity {
     @Override
     protected void onPostExecute(Void result){
         //setup our Adapter and set it to the ListView.
+        posant = posactual;
         mAdapter = new AdaptadorClass(VideosActivity.this, -1, GatvXmlParser.getStackSitesFromFile(VideosActivity.this));
         entryList.setAdapter(mAdapter);
         Log.i("StackSites", "adapter size = " + mAdapter.getCount());
         Log.i("StackSites", "Server value = " + server);
+        posactual = mAdapter.getCount();
         if (!server){
-            displayAlert();
+            displayAlert();}
+        Log.d("Pos.actual", Integer.toString(posactual));
+        if ((posactual - posant)>0) {
+                alarm();
         }
+
 
     }
 }
@@ -144,7 +158,7 @@ public class VideosActivity extends  AppCompatActivity {
         startActivity(intent);
     }
 
-    public  void refreshButton(){
+    public void refreshButton(){
         Intent intent = getIntent();
         overridePendingTransition(android.R.anim.cycle_interpolator, android.R.anim.cycle_interpolator);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
@@ -169,6 +183,33 @@ public class VideosActivity extends  AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
 
+    }
+
+    private void alarm(){
+        SharedPreferences getAlarms = PreferenceManager.getDefaultSharedPreferences(this);
+        String alarms = getAlarms.getString("notifications_new_message_ringtone", "default ringtone");
+        Uri uri = Uri.parse(alarms);
+        playSound(this, uri);
+
+        //call mMediaPlayer.stop(); when you want the sound to stop
+    }
+
+
+
+    private void playSound(Context context, Uri alert) {
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(context, alert);
+            final AudioManager audioManager = (AudioManager) context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+                mMediaPlayer.prepare();
+                mMediaPlayer.start();
+            }
+        } catch (IOException e) {
+            System.out.println("OOPS");
+        }
     }
 
 
